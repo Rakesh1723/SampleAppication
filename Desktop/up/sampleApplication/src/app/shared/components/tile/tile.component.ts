8c93ddc78390121a2ddd85625b7aa0af9750cca8
rakesh <ageerurakesh@gmail.com>
2025-05-30 09:18:05 +0530

    changed

--- Desktop/up/sampleApplication/src/app/shared/components/tile/tile.component.ts
+++ Desktop/up/sampleApplication/src/app/shared/components/tile/tile.component.ts
@@ -5,22 +5,30 @@ import { CommonModule } from '@angular/common';
   selector: 'app-tile',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <div class="tile" [ngClass]="[variant ? 'tile-' + variant : '', size ? 'tile-' + size : '']">
-      <div class="tile-header">
-        <h3 class="tile-title">{{ title }}</h3>
-        <p *ngIf="subtitle" class="tile-subtitle">{{ subtitle }}</p>
-      </div>
-      <div class="tile-content">
-        <ng-content></ng-content>
-      </div>
-    </div>
-  `,
+  templateUrl: './tile.component.html',
   styleUrls: ['./tile.component.scss']
 })
 export class TileComponent {
   @Input() title: string = '';
-  @Input() subtitle?: string;
-  @Input() variant?: 'primary' | 'success' | 'warning' | 'danger';
-  @Input() size?: 'sm' | 'lg';
+  @Input() status: string = '';
+  @Input() field1Label: string = '';
+  @Input() field1Value: string = '';
+  @Input() field2Label: string = '';
+  @Input() field2Value: string = '';
+  @Input() field3Label: string = '';
+  @Input() field3Value: string = '';
+
+  isActive: boolean = false;
+
+  onMouseDown() {
+    this.isActive = true;
+  }
+
+  onMouseUp() {
+    // Remove this method as we don't want to deactivate on mouse up
+  }
+
+  onMouseLeave() {
+    this.isActive = false;
+  }
 } 
